658d9f68f4599c1efb538ad25ef0b6d3b8317459
Rishikesh T <rishikeshtgit@gmail.com>
2023-10-03 16:47:13 +0530

    Create User Model

---
+++ PropertyPulse_Backend/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { stringify } from "querystring";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is Required"],
+      unique: [true, "Username must be Unique"],
+    },
+    email: {
+      type: String,
+      required: [true, "email is Required"],
+      unique: [true, "email must be Unique"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is Required"],
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
