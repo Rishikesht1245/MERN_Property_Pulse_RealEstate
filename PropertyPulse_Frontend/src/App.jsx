5d072baabda9e56c2b058a9462e0f106ac93c9b1
Rishikesh T <rishikeshtgit@gmail.com>
2023-10-04 23:47:52 +0530

    Update the header and Made profile page private

--- PropertyPulse_Frontend/src/App.jsx
+++ PropertyPulse_Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import { Toaster } from "react-hot-toast";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
@@ -18,7 +19,9 @@ export default function App() {
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/about" element={<About />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
         </Routes>
       </Router>
     </>
--- PropertyPulse_Frontend/src/components/Header.jsx
+++ PropertyPulse_Frontend/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import { FaSearch } from "react-icons/fa";
 import { NavLink, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
   const location = useLocation();
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className="bg-slate-200 shadow-md sticky top-0 z-10">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -22,14 +24,14 @@ export default function Header() {
             <FaSearch className="text-slate-400 cursor-pointer hover:text-slate-600" />
           </button>
         </form>
-        <ul className="flex gap-6 tracking-wider">
+        <ul className="flex gap-6 tracking-wider items-center">
           <li>
             <NavLink
               to="/"
               className={(navClass) =>
                 navClass.isActive
-                  ? "text-red-600 text-[16px] font-semibold hidden sm:inline"
-                  : "text-black text-[16px] font-semibold hidden sm:inline"
+                  ? "text-slate-700 text-[16px] font-semibold hidden sm:inline hover:underline"
+                  : "text-slate-500 text-[16px] font-semibold hidden sm:inline hover:underline"
               }
             >
               Home
@@ -40,24 +42,34 @@ export default function Header() {
               to="/about"
               className={(navClass) =>
                 navClass.isActive
-                  ? "text-red-600 text-[16px]  font-semibold hidden sm:inline"
-                  : "text-black text-[16px] font-semibold hidden sm:inline"
+                  ? "text-slate-700 text-[16px]  font-semibold hidden sm:inline hover:underline"
+                  : "text-slate-500 text-[16px] font-semibold hidden sm:inline hover:underline"
               }
             >
               About
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to="/sign-in"
-              className={(navClass) =>
-                navClass.isActive
-                  ? "text-red-600  sm:text-[16px] font-semibold"
-                  : "text-black  sm:text-[16px] font-semibold"
-              }
-            >
-              Sign In
-            </NavLink>
+            {currentUser ? (
+              <NavLink to="/profile">
+                <img
+                  className="rounded-full h-9 w-9 object-cover"
+                  src={currentUser.avatar}
+                  alt=""
+                />
+              </NavLink>
+            ) : (
+              <NavLink
+                to="/sign-in"
+                className={(navClass) =>
+                  navClass.isActive
+                    ? "text-slate-700  sm:text-[16px] font-semibold hover:underline"
+                    : "text-slate-500  sm:text-[16px] font-semibold hover:underline"
+                }
+              >
+                Sign In
+              </NavLink>
+            )}
           </li>
         </ul>
       </div>
