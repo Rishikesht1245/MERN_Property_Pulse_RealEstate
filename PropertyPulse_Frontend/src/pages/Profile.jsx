eb5a3d4ae7077a5ddb1897c140eb227fb56c7d71
Rishikesh T <rishikeshtgit@gmail.com>
2023-10-05 00:10:24 +0530

    Complete profile page UI

--- PropertyPulse_Frontend/src/pages/Profile.jsx
+++ PropertyPulse_Frontend/src/pages/Profile.jsx
@@ -1,4 +1,85 @@
+import { Formik, Form } from "formik";
+import { registerSchema } from "../schema/authSchema";
+import Input from "../components/subcomponents/Input";
+import PasswordInput from "../components/subcomponents/PasswordInput";
+import Button from "../components/subcomponents/Button";
+import { useSelector } from "react-redux";
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="sm:max-w-lg w-[90%] mx-auto my-10">
+      <h1 className="uppercase tracking-widest text-center text-3xl font-semibold my-5">
+        Profile
+      </h1>
+      <Formik
+        initialValues={{
+          username: currentUser.username,
+          email: currentUser.email,
+          password: "",
+          confirmPassword: "",
+        }}
+        validationSchema={registerSchema}
+        onSubmit={(formData, { setSubmitting }) => {
+          setSubmitting(true);
+          console.log(formData);
+          registerUser(formData)
+            .then(({ data }) => {
+              toast.success("Registration Successful!", {
+                style: {
+                  borderRadius: "10px",
+                  background: "#333",
+                  color: "#fff",
+                },
+              });
+              navigate("/sign-in");
+              return;
+            })
+            // inside error, response will be there inside it the actual error message sent from backend will see
+            .catch(
+              ({
+                response: {
+                  data: { message },
+                },
+              }) => {
+                console.log(message);
+                setError(message);
+              }
+            )
+
+            .finally(() => setSubmitting(false));
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className="flex flex-col justify-center gap-4 px-1 mb-3">
+            <img
+              src={currentUser.avatar}
+              alt=""
+              className="rounded-full h-24 w-24 object-cover self-center cursor-pointer my-5"
+            />
+            <Input type="text" placeholder="User Name" name="username" />
+            <Input type="email" placeholder="Email" name="email" />
+            <PasswordInput
+              type="password"
+              placeholder="Update Password"
+              name="password"
+              id="password"
+            />
+
+            <Button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? "Updating..." : "Update"}
+            </Button>
+          </Form>
+        )}
+      </Formik>
+      <div className="flex justify-between px-1">
+        <span className="text-red-600 font-semibold cursor-pointer tracking-wide">
+          Delete account
+        </span>
+        <span className="text-red-600 font-semibold cursor-pointer tracking-wide">
+          Sign out
+        </span>
+      </div>
+    </div>
+  );
 };
 export default Profile;
