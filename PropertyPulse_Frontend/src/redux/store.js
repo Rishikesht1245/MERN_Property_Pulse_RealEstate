e4943caae4c14f6ba9aa718dd2c8da5d955d5a3d
Rishikesh T <rishikeshtgit@gmail.com>
2023-10-04 17:37:55 +0530

    Add redux-persist

--- PropertyPulse_Frontend/src/redux/store.js
+++ PropertyPulse_Frontend/src/redux/store.js
@@ -1,10 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+
+//creating the root reducer
+const rootReducer = combineReducers({ user: userReducer });
+
+// configuring the persist
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+// create the persisted reducer from persistConfig and rootReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
