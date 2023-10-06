623d301fc4d382440858f07cbed2464be5adb348
Rishikesh T <rishikeshtgit@gmail.com>
2023-10-06 19:04:07 +0530

    Create update listing api route

--- PropertyPulse_Backend/routes/user/listing.routes.js
+++ PropertyPulse_Backend/routes/user/listing.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createListing,
   deleteListing,
+  updateListing,
 } from "../../controllers/user/listing.controller.js";
 import { verifyToken } from "../../utils/verifyUser.js";
 
@@ -11,5 +12,8 @@ router.post("/create", verifyToken, createListing);
 
 // delete listing
 router.delete("/delete/:id", verifyToken, deleteListing);
+
+// edit listing
+router.post("/update/:id", verifyToken, updateListing);
 
 export default router;
