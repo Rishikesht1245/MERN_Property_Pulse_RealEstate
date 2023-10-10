5a153cf572d16addf95422a1c6c23b2eea84e603
Rishikesh T <rishikeshtgit@gmail.com>
2023-10-10 16:38:47 +0530

    Create messenger UI

--- PropertyPulse_Frontend/src/pages/Messenger.jsx
+++ PropertyPulse_Frontend/src/pages/Messenger.jsx
@@ -1,4 +1,21 @@
+import Chats from "../components/Chats";
+import PropertyDetails from "../components/PropertyDetails";
+import MessageBox from "./MessageBox";
+
 const Messenger = () => {
-  return <div>Messenger</div>;
+  return (
+    <div className="flex justify-center gap-5 p-10 h-[calc(100vh-40px)]">
+      <div className="flex w-1/4 flex-col">
+        <Chats />
+      </div>
+      <div className="flex flex-col w-1/2">
+        {" "}
+        <MessageBox />
+      </div>
+      <div className="flex flex-col w-1/4">
+        <PropertyDetails />
+      </div>
+    </div>
+  );
 };
 export default Messenger;
